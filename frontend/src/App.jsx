e30ed1ae8cdff98f60e5a76e4c0abf0b650f7227
Francis <32603796+fsilva7456@users.noreply.github.com>
2024-12-18 15:39:29 -0500

    Add basic React frontend and Dockerfile

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Object Finder</h1>
+        <p>Welcome to the Object Detection System</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
